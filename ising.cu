8c9b4e583caef020ab7ba0000a05953b16aa50ab
Gregorio Vilardo <gregovilardo@gmail.com>
2025-06-20 11:32:58 -0300

    printtt

--- ising.cu
+++ ising.cu
@@ -1,5 +1,6 @@
 #include "ising.h"
 
+#include <cstdio>
 #include <math.h>
 #include <stdlib.h>
 
@@ -7,8 +8,9 @@ __device__ inline int get_value(int *d_grid, size_t pitch, int i, int j) {
   return *((int *)((char *)d_grid + i * pitch) + j);
 }
 
-__device__ unsigned int xor128(int seed) {
-  unsigned int x = seed + blockIdx.x * 123456789 + threadIdx.x * 987654321;
+__device__ unsigned int xor128() {
+  // unsigned int x = seed + blockIdx.x * 123456789 + threadIdx.x * 987654321;
+  unsigned int seed = threadIdx.x + blockIdx.x * blockDim.x;
   x ^= x << 13;
   x ^= x >> 17;
   x ^= x << 5;
@@ -31,6 +33,12 @@ __global__ void update(const float temp, int *d_grid, size_t pitch) {
 
     int h_before = -(spin_old * spin_n) - (spin_old * spin_e) -
                    (spin_old * spin_w) - (spin_old * spin_s);
+    if (i == 4) {
+      printf("spin_n: %d\n", spin_n);
+      printf("spin_s: %d\n", spin_s);
+      printf("spin_e: %d\n", spin_e);
+      printf("spin_w: %d\n", spin_w);
+    }
 
     // h after taking new spin
     int h_after = -(spin_new * spin_n) - (spin_new * spin_e) -
@@ -39,11 +47,17 @@ __global__ void update(const float temp, int *d_grid, size_t pitch) {
     int delta_E = h_after - h_before;
     // float p = rand() / (float)RAND_MAX;
 
-    int frame_seed = 0x912; // TODO: changue this
-    unsigned int rand_int = xor128(i + frame_seed * 999999937);
+    // int frame_seed = 0x912; // TODO: changue this
+    unsigned int rand_int = xor128();
     float p = (rand_int & 0xFFFF) / 65535.0f;
+    if (i == 4) {
+      printf("p: %d\n", p);
+      printf("delta_E: %d\n", delta_E);
+    }
 
     if (delta_E <= 0 || p <= __expf(-delta_E / temp)) {
+
+      printf("HOLA@\n");
       int *grid_i_j = (int *)((char *)d_grid + i * pitch) + j;
       *grid_i_j = spin_new;
     }
